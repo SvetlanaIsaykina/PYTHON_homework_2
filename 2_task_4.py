402af7e192bf6ce3d7edb5306f15a8901d39f6ce
Svetlana Isaykina <svet4731@gmail.com>
2023-01-10 18:48:41 +0300

    задача № 4

--- 2_task_4.py
+++ 2_task_4.py
@@ -1,2 +1,15 @@
 # Задача 4. Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
 # Сдвиньте все элементы списка на 2 позиции вправо.
+
+num_n = int(input('Введите число N: '))
+num_x = - num_n
+numbers = []
+
+while num_x <= num_n:
+    numbers.append(num_x)
+    num_x += 1
+print(numbers)
+
+numbers = numbers[-2:] + numbers[:-2]
+
+print(numbers)
